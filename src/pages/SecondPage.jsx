25ec36869e025c497ba55e1945641d6ccf4bfd62
makha <tnysovm@gmail.com>
2026-04-13 11:39:46 +0500

    last last lastееeeууeeуsdsaddddddddd

--- src/pages/SecondPage.jsx
+++ src/pages/SecondPage.jsx
@@ -9,7 +9,7 @@ export default function SecondPage() {
   const categories = [
     { title: t.furniture_divany,   img: '/img/pagesecond/a1a32584672.png',   path: '/secondpage/divany' },
     { title: t.furniture_kreslo,   img: '/img/pagesecond/738d1eff.png',      path: '/secondpage/kreslo' },
-    { title: t.furniture_kuhnya,   img: '/img/pagesecond/4a950c62bed144.jpg', path: '/secondpage/kuxnya' },
+    { title: t.furniture_kuhnya,   img: '/img/pagesecond/4a950c62bed144.jpg', path: '/secondpage/kuhnya' },
     { title: t.furniture_pufy,     img: '/img/pagesecond/a41bcac159.png',    path: '/secondpage/pufy' },
     { title: t.furniture_stellazhi,img: '/img/pagesecond/e0f5951d3c3.png',   path: '/secondpage/stellazhi' },
     { title: t.furniture_stoly,    img: '/img/pagesecond/bb20aa.png',        path: '/secondpage/stoly' },
